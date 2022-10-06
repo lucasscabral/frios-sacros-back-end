2338368014ea43d4a29307b5a4684c9fc56832b7
Lucas Cabral <lucas.9cabral@gmail.com>
2022-10-06 02:29:02 -0300

    fix: Altera o parametro req.

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import * as categoriesService from "../services/categoriesService"
 
-export async function getAllCategories(req: Request, res: Response) {
+export async function getAllCategories(_: Request, res: Response) {
     const allCategories = await categoriesService.getAllCategories()
 
     res.status(200).send(allCategories)
