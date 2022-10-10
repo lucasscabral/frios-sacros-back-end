a1f15d6db1e0c62cd7318eb307624f8a15be398c
Lucas Cabral <lucas.9cabral@gmail.com>
2022-10-10 14:00:23 -0300

    fix:Ajusta script dev

--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import * as productsController from "../controllers/productsController"
+import { validarToken } from "../middlewers/validateToken";
 
 const productsRouter = Router()
 
+// productsRouter.use(validarToken)
 productsRouter.get("/products", productsController.getAllProducts)
 
 export default productsRouter
